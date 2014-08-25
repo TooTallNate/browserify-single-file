2d418ae410b36164fef9cfb7bb187d1d8ce796bf
Nathan Rajlich <nathan@tootallnate.net>
2014-08-25 12:10:59 -0700

    bin: add `--plugin/-p` support

--- bin/browserify-transform.js
+++ bin/browserify-transform.js
@@ -5,17 +5,43 @@
  */
 
 var fs = require('fs');
+var events = require('events');
 var minimist = require('minimist');
 
 var argv = minimist(process.argv.slice(2));
 
-var transform = argv.transform || argv.t;
 var filenames = argv._;
 if (filenames.length !== 1) {
   throw new Error('only one source filename supported at a time');
 }
 var filename = filenames[0];
 
-var t = require(transform);
+var source = fs.createReadStream(filename);
+var next = source;
 
-fs.createReadStream(filename).pipe(t(filename)).pipe(process.stdout);
+// emulate browserify's API a little bit
+var bundle = new events.EventEmitter();
+bundle._extensions = Object.keys(require.extensions);
+bundle._entries = [ filename ];
+bundle.transform = function (t) {
+  next = next.pipe(t(filename));
+};
+
+// apply any "transform"
+var transform = argv.transform || argv.t;
+if (transform) {
+  var t = require(transform);
+  bundle.transform(t);
+}
+
+// apply any "plugin"
+var plugin = argv.plugin || argv.p;
+if (plugin) {
+  var p = require(plugin);
+  p(bundle);
+}
+
+bundle.emit('bundle');
+
+// output the pipeline to `stdout`
+next.pipe(process.stdout);
