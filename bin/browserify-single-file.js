a50a93266a51a64a7d0f9e9dcfe7bf724ca2d756
Nathan Rajlich <nathan@tootallnate.net>
2014-08-27 11:32:37 -0700

    bin: add support for multiple --transform/--plugin arguments
    So that you can stack, i.e. coffeeify, and then deamdify transforms
    on top of eachother.

--- bin/browserify-single-file.js
+++ bin/browserify-single-file.js
@@ -17,21 +17,51 @@ var filename = filenames.shift();
 
 var bsf = new BrowserifySingleFile(filename);
 
-// apply any "transform"
-var transform = argv.transform || argv.t;
-if (transform) {
+// apply any "transform" parameters
+var transforms = [];
+if (argv.transform) {
+  if (Array.isArray(argv.transform)) {
+    transforms.push.apply(transforms, argv.transform);
+  } else {
+    transforms.push(argv.transform);
+  }
+}
+if (argv.t) {
+  if (Array.isArray(argv.t)) {
+    transforms.push.apply(transforms, argv.t);
+  } else {
+    transforms.push(argv.t);
+  }
+}
+transforms.forEach(function (transform) {
   var t = require(transform);
   bsf.transform(t);
-}
+});
+
 
 // apply any "plugin"
-var plugin = argv.plugin || argv.p;
-if (plugin) {
+var plugins = [];
+if (argv.plugin) {
+  if (Array.isArray(argv.plugin)) {
+    plugins.push.apply(plugins, argv.plugin);
+  } else {
+    plugins.push(argv.plugin);
+  }
+}
+if (argv.p) {
+  if (Array.isArray(argv.p)) {
+    plugins.push.apply(plugins, argv.p);
+  } else {
+    plugins.push(argv.p);
+  }
+}
+plugins.forEach(function (plugin) {
   var pargv = minimist(plugin.split(/\s+/));
   var pname = pargv._[0];
   var p = require(pname);
   p(bsf, pargv);
-}
+});
+
 
 // output the pipeline to `stdout`
 bsf.bundle().pipe(process.stdout);
